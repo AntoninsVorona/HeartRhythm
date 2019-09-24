4ecd85f5ea3510bb498a44caf94501c74458153d
bubucha <antoninsvorona@gmail.com>
2019-09-24 16:17:51 +0300

    * No filtering and compression for new sprites

---
+++ Assets/Scripts/Utilities/AssetProcessing/PostprocessImages.cs
@@ -0,0 +1,12 @@
+using UnityEditor;
+using UnityEngine;
+
+public class PostprocessImages : AssetPostprocessor
+{
+	void OnPostprocessTexture(Texture2D texture)
+	{
+		TextureImporter textureImporter = (TextureImporter) assetImporter;
+		textureImporter.filterMode = FilterMode.Point;
+		textureImporter.textureCompression = TextureImporterCompression.Uncompressed;
+	}
+}
