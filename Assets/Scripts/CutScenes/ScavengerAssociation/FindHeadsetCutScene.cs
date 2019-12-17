c2c6b6d19c8d980f9facde3bd317850686f8a8f8
bubucha <antoninsvorona@gmail.com>
2019-12-17 15:34:49 +0200

    * Find Headset Cut Scene

--- Assets/Scripts/CutScenes/ScavengerAssociation/FindHeadsetCutScene.cs
+++ Assets/Scripts/CutScenes/ScavengerAssociation/FindHeadsetCutScene.cs
@@ -8,14 +8,6 @@ public class FindHeadsetCutScene : CutScene
 	{
 		dialogueFinished = false;
 		//TODO Play Search Animation and Sound
-//		Player.Instance.Move(new Vector2Int(-5, 12), true);
-//		yield return new WaitForSeconds(1);
-//		yield return Player.Instance.Move(new Vector2Int(-5, 13));
-//		yield return new WaitForSeconds(0.5f);
-//		yield return Player.Instance.Move(new Vector2Int(-5, 14));
-//		yield return new WaitForSeconds(0.5f);
-//		yield return Player.Instance.Move(new Vector2Int(-5, 15));
-//		yield return new WaitForSeconds(0.5f);
 		GameLogic.Instance.StartConversation("FoundHeadsetFirstTime");
 		yield return new WaitUntil(() => dialogueFinished);
 		Player.Instance.PickUpItem(ItemManager.Instance.GetItemByName("Headset"));
