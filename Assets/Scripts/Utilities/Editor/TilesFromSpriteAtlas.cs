595a0888888e2b0808c0a2ade4ca1407d3d7e83a
bubucha <antoninsvorona@gmail.com>
2020-02-27 17:56:06 +0200

    * Dirty Floor (Scavenger Field) Tileset

---
+++ Assets/Scripts/Utilities/Editor/TilesFromSpriteAtlas.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using UnityEditor;
+using UnityEngine;
+
+public class TilesFromSpriteAtlas : EditorWindow
+{
+	public RuleTile prefab;
+	public List<Sprite> sprites;
+
+	[MenuItem("Window/bubucha/TilesFromSpriteAtlas")]
+	private static void Init()
+	{
+		GetWindow(typeof(TilesFromSpriteAtlas)).Show();
+	}
+
+	private void OnGUI()
+	{
+		ScriptableObject target = this;
+		SerializedObject so = new SerializedObject(target);
+		SerializedProperty prefabProperty = so.FindProperty("prefab");
+		EditorGUILayout.PropertyField(prefabProperty, false);
+		SerializedProperty spritesProperty = so.FindProperty("sprites");
+		EditorGUILayout.PropertyField(spritesProperty, true);
+		so.ApplyModifiedProperties();
+		if (GUILayout.Button("Convert"))
+		{
+			var counter = -1;
+			var assetPath = AssetDatabase.GetAssetPath(prefab);
+			var directoryName = Path.GetDirectoryName(assetPath);
+			var prefabName = prefab.name;
+			sprites.ForEach(s =>
+			{
+				var tile = Instantiate(prefab);
+				tile.m_DefaultSprite = s;
+				tile.m_TilingRules.First().m_Sprites[0] = s;
+				AssetDatabase.CreateAsset(tile, $"{directoryName}\\{prefabName}{++counter}.asset");
+			});
+
+			AssetDatabase.SaveAssets();
+		}
+	}
+}
