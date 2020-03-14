46db6ca23e544a4da6684339210afed780c2c359
bubucha <antoninsvorona@gmail.com>
2020-03-14 03:29:31 +0200

    * Meme Bum

---
+++ Assets/Scripts/Units/Interactions/LocationSpecific/ScavengerAssociation/MemeBumInteraction.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using UnityEngine;
+
+[Serializable]
+[CreateAssetMenu(menuName = "Interactions/Location Specific/Scavenger Association/Meme Bum Interaction",
+	fileName = "MemeBumInteraction")]
+public class MemeBumInteraction : Interaction
+{
+	public override bool ApplyInteraction()
+	{
+		Player.Instance.Talk("You ok?");
+		owner.Talk("Ok.");
+		return true;
+	}
+
+	protected override object[] GetDescriptionParams()
+	{
+		return new object[0];
+	}
+}
